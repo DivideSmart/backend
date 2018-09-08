684d9132710fc818c5ed77e8baa07c78ba1cc4ae
Le Trung Hieu <lth08091998@gmail.com>
2018-09-09 00:02:20 +0800

    Add color-constrasting methods + hypertune parameters

--- dividesmart/python_scripts/preprocess.py
+++ dividesmart/python_scripts/preprocess.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 import argparse
 from transform import *
-import pytesseract
+from tesserocr import PyTessBaseAPI
 
 # Just a helper function to help visualize image
 def combine(_height, *args):
@@ -24,15 +24,16 @@ def combine(_height, *args):
 
 # Just a helper function to help visualize image
 def show(img):
-    cv2.imshow('Basic Image', img)
-    cv2.waitKey(0)
+    return 1
+    # cv2.imshow('Basic Image', img)
+    # cv2.waitKey(0)
 
 def resize(img, _size):
     ratio = float(_size) / img.shape[0]
     dim   = (int(img.shape[1] * ratio), _size)
     return cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
 
-def preprocess(img):
+def preprocess(img, clahe, noise_level):
     args = {'height': 300, 'closing': 3}
     # Getting the user input
     HEIGHT              = int(args['height'])
@@ -49,9 +50,18 @@ def preprocess(img):
 
     total_area = image.shape[0] * image.shape[1]
 
-    def scan():
+    def scan(clahe, noise_level):
         """ Step 1: Edge Detection """
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # get the grayscale image
+
+        # Let use Clahe or Histogram equalization for color constrast
+        # This will better the result of Edge Detection when the background have similar 
+        # color to the Receipt
+        if(clahe == 1):
+            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+            gray = clahe.apply(gray)
+        elif(clahe == 2):
+            gray = cv2.equalizeHist(gray)
         gray = cv2.bilateralFilter(gray, 11, 17, 17)
 
         # automatic Canny edge detection thredhold computation
@@ -75,8 +85,8 @@ def preprocess(img):
             approx  = cv2.approxPolyDP(contour, 0.02 * peri, True)
             area    = cv2.contourArea(contour)
 
-            # we don't consider anything less than 10% of the whole image
-            if area < 0.01 * total_area:
+            # we don't consider anything less than 20% of the whole image
+            if area < 0.2 * total_area:
                 continue
             # if the approximated contour has 4 points, then assumer it is a receipt
             # a book is a receipt and thus it has 4 vertices
@@ -108,7 +118,7 @@ def preprocess(img):
 
                 # The higher the parameter of medianBlur, the less noisy but text will get blurred
                 # Need to tune a bit.
-                scan_warped = cv2.medianBlur(np.array(scan_warped), 1)
+                scan_warped = cv2.medianBlur(np.array(scan_warped), noise_level)
                 scan_warped = cv2.adaptiveThreshold(scan_warped, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
                 # save the image
@@ -118,28 +128,53 @@ def preprocess(img):
                 cv2.imwrite(filename_scan, scan_warped)
                 show(scan_warped)
 
+                with PyTessBaseAPI() as api:
+                    api.SetImageFile(filename_scan)
+                    return api.GetUTF8Text(), np.mean(np.array(api.AllWordConfidences()))
                 # Without these steps of pre-processing (especially the findContour part),
                 # the return result of pytesseract is empty.
-                return pytesseract.image_to_string(scan_warped)
 
         # The "else" case is when cannot detect any receipt, now used for debugging only
-        else:
+        else:   
+                print("ELSE CASE")
                 top_row = combine(300, original, gray)
                 bot_row = combine(300, closed, image)
                 show(top_row)
                 show(bot_row)
                 scan_warped = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
 
+                scan_warped = cv2.medianBlur(np.array(scan_warped), noise_level)
                 scan_warped = cv2.adaptiveThreshold(scan_warped, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
                 # save the image
                 filename_color = './results/scan%03d_color.jpg' % total
                 filename_scan  = './results/scan%03d_scan.jpg' % total
                 cv2.imwrite(filename_scan, scan_warped)
-
+                show(scan_warped)
+                with PyTessBaseAPI() as api:
+                    api.SetImageFile(filename_scan)
+                    return api.GetUTF8Text(), np.mean(np.array(api.AllWordConfidences()))
 
         return total
 
-    return scan()
+    return scan(clahe, noise_level)
 
-result = preprocess('./img/example4.jpeg')
+def choose_best_preprocess(img):
+    final_content = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    final_confidence = 0
+    for i in [0, 1, 2]:
+        for j in [1, 3, 5, 7, 9]:
+            content, conf = preprocess(img, i, j)
+
+            # Have to check whether the content is empty or not, because 
+            # it may not be able to detect anything but still return a very high confidence level.
+            if(conf > final_confidence and len(content) > 0.8 * len(final_content)):
+                print("BEST")
+                print(conf)
+                print(content)
+                final_confidence = conf
+                final_content = content
+    return final_content, final_confidence
+
+result, confidence = choose_best_preprocess('./img/example1.jpg')
 print(result)
+print(confidence)
