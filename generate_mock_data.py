0e2a9fe0a41bda0de87dd1b368f4dcd9c19d6b7a
royl8 <royl8@qq.com>
2018-09-11 10:05:02 +0800

    update the script

--- generate_mock_data.py
+++ generate_mock_data.py
@@ -7,3 +7,15 @@ django.setup()
 
 
 from main import models
+
+for user in models.User.objects.all():
+    user.delete()
+
+# for i in range(1,11):
+#     mock_user = models.User(
+#         username='org'+str(i),
+#         email_address=str(i)+'@org.com',
+#         user_type=2,
+#         system_config=config,
+#     )
+#     mock_user.save()
