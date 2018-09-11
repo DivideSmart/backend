59e1de5fabc8a86e6673c96a96e23ca8052a9c06
Edmund Mok <edmundmok@outlook.com>
2018-09-11 23:16:10 +0800

    Update fixtures

--- dividesmart/fixtures.py
+++ dividesmart/fixtures.py
@@ -1,6 +1,8 @@
 import os, sys
 import django
 
+from collections import namedtuple
+
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE', 'dividesmart.settings'
 )
@@ -8,9 +10,29 @@ django.setup()
 
 from main.models import *
 
+UserT = namedtuple('UserT', ['username', 'email_address', 'password'])
 
-User.objects.create_user(
-    username='John Smith',
-    email_address='johnsmith@gmail.com',
-    password='johnsmith123'
-)
+USERS = [
+    UserT(
+        username='John Smith',
+        email_address='johnsmith@gmail.com',
+        password='johnsmith123'
+    ),
+    UserT(
+        username='Jane Doe',
+        email_address='janedoe@gmail.com',
+        password='janedoe94'
+    ),
+    UserT(
+        username='Bill Gates',
+        email_address='billgates@outlook.com',
+        password='bgates'
+    )
+]
+
+for user in USERS:
+    User.objects.create_user(
+        username=user.username,
+        email_address=user.email_address,
+        password=user.password
+    )
