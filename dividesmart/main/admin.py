9bb0e79725f9a4b872434b9ba7f10069d6428958
royl8 <royl8@qq.com>
2018-09-12 10:50:51 +0800

    add admin to User and Session model

--- dividesmart/main/admin.py
+++ dividesmart/main/admin.py
@@ -1,6 +1,22 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from django.contrib.sessions.models import Session
+# from .admin_django_auth import *
 
-# Register your models here.
+from . import models
+
+
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email_address', 'is_superuser', 'is_staff']
+    list_filter = ['is_superuser', 'is_staff']
+    search_fields = ['id', 'username', 'email_address']
+    filter_horizontal = ['groups', 'user_permissions']
+
+
+class SessionAdmin(admin.ModelAdmin):
+    def _session_data(self, obj):
+        return obj.get_decoded()
+    list_display = ['session_key', '_session_data', 'expire_date']
+
+
+admin.site.register(Session, SessionAdmin)
+admin.site.register(models.User, UserModelAdmin)
