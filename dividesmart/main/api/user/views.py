1e9f6e3f14377dd8911c128c1f34149edcbd795a
royl8 <royl8@qq.com>
2018-09-12 11:07:03 +0800

    use encoder maybe. the serializer is currently broken

--- dividesmart/main/api/user/views.py
+++ dividesmart/main/api/user/views.py
@@ -1,15 +1,19 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseForbidden, JsonResponse
-from django.core import serializers
 from django.contrib.auth import get_user
+from django.core import serializers
+from django.http import (HttpResponse, HttpResponseForbidden,
+                         HttpResponseNotFound, JsonResponse)
+from django.views.decorators.csrf import csrf_exempt
 
 
 def user(request):
     current_user = get_user(request)
     print(current_user)
     if not current_user.is_authenticated:
+        print('test')
         return HttpResponseForbidden('Not logged in')
-    user_json = serializers.serialize(
-        'json', request.user,
-        fields=('email_address', 'username', 'date_joined', 'balance')
-    )
+    # user_json = serializers.serialize(
+    #     'json', request.user,
+    #     fields=('email_address', 'username', 'date_joined', 'balance')
+    # )
+    user_json = {}
     return JsonResponse(user_json, status=200)
