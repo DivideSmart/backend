196f5971aa8778d975e325ff14f03f481ee90e19
Edmund Mok <edmundmok@outlook.com>
2018-09-12 16:06:51 +0800

    Return user as json

--- dividesmart/main/api/user/views.py
+++ dividesmart/main/api/user/views.py
@@ -1,19 +1,16 @@
 from django.contrib.auth import get_user
-from django.core import serializers
-from django.http import (HttpResponse, HttpResponseForbidden,
-                         HttpResponseNotFound, JsonResponse)
-from django.views.decorators.csrf import csrf_exempt
+from django.http import (
+    HttpResponseForbidden, JsonResponse
+)
+from django.forms.models import model_to_dict
 
 
 def user(request):
     current_user = get_user(request)
-    print(current_user)
     if not current_user.is_authenticated:
-        print('test')
         return HttpResponseForbidden('Not logged in')
-    # user_json = serializers.serialize(
-    #     'json', request.user,
-    #     fields=('email_address', 'username', 'date_joined', 'balance')
-    # )
-    user_json = {}
+    user_json = model_to_dict(
+        current_user,
+        fields=('email_address', 'username', 'date_joined', 'balance')
+    )
     return JsonResponse(user_json, status=200)
