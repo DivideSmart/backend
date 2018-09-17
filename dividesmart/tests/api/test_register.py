b0b2a6ac98cb868f8d4d15ed6032a2526c79f380
Edmund Mok <edmundmok@outlook.com>
2018-09-17 22:46:03 +0800

    Add User objects assertion in register test

--- dividesmart/tests/api/test_register.py
+++ dividesmart/tests/api/test_register.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from tests.api.test_utils import *
+from main.models import User
 
 
 class RegistrationTest(TestCase):
@@ -9,6 +10,8 @@ class RegistrationTest(TestCase):
     TEST_PASSWORD = 'johnsmith123'
 
     def test_register_new_account(self):
+        assert not User.objects.filter(email_address=self.TEST_EMAIL).exists()
+
         # first attempt to login but expect failure
         response = self.client.post(LOGIN_URL, {
             'email_address': self.TEST_EMAIL,
@@ -25,6 +28,8 @@ class RegistrationTest(TestCase):
         assert response.status_code == 200
         json_response = response.json()
         assert json_response['success']
+
+        assert User.objects.filter(email_address=self.TEST_EMAIL).exists()
 
         # login and expect success
         response = self.client.post(LOGIN_URL, {
