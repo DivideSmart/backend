35fa18ce97f54b3bc415336ba88aa1bf1b201ee5
Sreyans Sipani <sreyans.sipani99@gmail.com>
2018-09-24 05:52:28 +0800

    Login page

--- dividesmart/main/static/login.js
+++ dividesmart/main/static/login.js
@@ -1,9 +1,7 @@
 import 'regenerator-runtime/runtime'
-import './style/index.css'
 import 'typeface-roboto'
 import 'antd-mobile/dist/antd-mobile.css'
 import 'util.js'
-import loginStyles from './login.css'
 
 import { CSSTransition, TransitionGroup } from "react-transition-group"
 import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom'
@@ -11,7 +9,7 @@ import { Provider, connect } from 'react-redux';
 import { faChevronLeft, faDollarSign, faHome, faReceipt, faUserCircle, faUsers } from '@fortawesome/free-solid-svg-icons'
 import { logoutUser, setCurrentUser } from './redux/actions/authActions.js';
 
-import { LocaleProvider } from 'antd-mobile';
+import { LocaleProvider, NavBar } from 'antd-mobile';
 import React from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
@@ -20,7 +18,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import store from './redux/store';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import { LinearGradient } from 'expo';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
 
 library.add([faDollarSign, faReceipt, faHome, faChevronLeft, faUsers, faUserCircle])
 
@@ -29,81 +28,101 @@ class LoginPage extends React.Component {
 
   constructor(props) {
       super();
-      this.state = { isAuthenticated: false, user: null, token: ''};
+      this.state = { isAuthenticated: false, username: '', password: '', token: ''};
   }
 
   logout = () => {
-      this.setState({isAuthenticated: false, token: '', user: null})
-  };
-
-  facebookResponse = (response) => {
-      const tokenBlob = new Blob([JSON.stringify({access_token: response.accessToken}, null, 2)], {type : 'application/json'});
-      const options = {
-          method: 'POST',
-          body: tokenBlob,
-          mode: 'cors',
-          cache: 'default'
-      };
-      fetch('http://localhost:8000/api/auth/facebook', options).then(r => {
-          const token = r.headers.get('x-auth-token');
-          r.json().then(user => {
-              if (token) {
-                  this.setState({isAuthenticated: true, user, token})
-              }
-          });
-      })
-      response.setHeader('x-auth-token', response.accessToken);
-  };
-
-  googleResponse = (response) => {
-      const tokenBlob = new Blob([JSON.stringify({access_token: response.accessToken}, null, 2)], {type : 'application/json'});
-      const options = {
-          method: 'POST',
-          body: tokenBlob,
-          mode: 'cors',
-          cache: 'default'
-      };
-      fetch('http://localhost:8000/api/v1/auth/google', options).then(r => {
-          const token = r.headers.get('x-auth-token');
-          r.json().then(user => {
-              if (token) {
-                  this.setState({isAuthenticated: true, user, token})
-              }
-          });
-      })
+      this.setState({isAuthenticated: false, token: '', username: '', password: ''})
   };
 
   onFailure = (error) => {
     alert(error);
   }
 
+  handleClick(event) {
+    var apiBaseUrl = "http://localhost:4000/api/";
+    var self = this;
+    var payload={
+    "email":this.state.username,
+    "password":this.state.password
+    }
+    axios.post(apiBaseUrl+'login', payload)
+    .then(function (response) {
+    console.log(response);
+    if(response.data.code == 200){
+    console.log("Login successfull");
+    var uploadScreen=[];
+    uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
+    self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
+    }
+    else if(response.data.code == 204){
+    console.log("Username password do not match");
+    alert("username password do not match")
+    }
+    else{
+    console.log("Username does not exists");
+    alert("Username does not exist");
+    }
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
+  }
+
   render() {
     return (
-          <div className={loginStyles.login}>
-            <Button
-              style={{ backgroundColor:'#DD4B39', borderColor:'#DD4B39', marginLeft: 600, marginTop: 300, color: 'white' }}
-              className='login-form-button'
-              htmlType='button'
-              id='google-login'
-            >
-              <i className='fa fa-google' aria-hidden='true'></i>
-              {'  '}Log in with Google
-            </Button>
-            <br></br>
-            <Button
-              style={{ backgroundColor:'#3b5998', borderColor:'#3b5998', marginLeft: 600, marginTop: 16 , color: 'white' }}
-              className='login-form-button'
-              htmlType='button'
-              id='facebook-login'
-              onClick={this.facebookLogin}
-              loading={this.state.fbLoginButtonLoading}
-            >
-              <i className='fa fa-facebook-official' aria-hidden='true'></i>
-              {'  '}Log in with Facebook
-            </Button>
-          </div>
-        </LinearGradient>
-      </View>
+      <div>
+        <div style={{ height: '6vh', position: 'fixed', width: '100%', zIndex: 1000 }}>
+          <NavBar
+            style={{height: '100%'}}
+            mode="light"
+          >
+            Name of the Appp
+          </NavBar>
+        </div>
+        <TextField style={{ marginLeft: '45%', marginTop: '10%'}}
+           hintText="Enter your Username"
+           floatingLabelText="Username"
+           label="Username"
+           onChange = {(event,newValue) => this.setState({username:newValue})}
+        />
+        <br></br>
+        <TextField style={{ marginLeft: '45%', marginTop: 20}}
+           type="password"
+           hintText="Enter your Password"
+           floatingLabelText="Password"
+           label="Password"
+           onChange = {(event,newValue) => this.setState({password:newValue})}
+        />
+        <br></br>
+        <Button primary={true} style={{marginLeft: '48%', marginTop: 20}} onClick={(event) => this.handleClick(event)}>
+        {'  '}Submit
+        </Button>
+        <br></br>
+        <Button
+          style={{ backgroundColor:'#DD4B39', borderColor:'#DD4B39', marginLeft: '45%', marginTop: '5%', color: 'white' }}
+          className='login-form-button'
+          htmlType='button'
+          id='google-login'
+        >
+        <i className='fa fa-google' aria-hidden='true'></i>
+        {'  '}Log in with Google
+        </Button>
+        <br></br>
+        <Button
+          style={{ backgroundColor:'#3b5998', borderColor:'#3b5998', marginLeft: '45%', marginTop: 20 , color: 'white' }}
+          className='login-form-button'
+          htmlType='button'
+          id='facebook-login'
+          onClick={this.facebookLogin}
+          loading={this.state.fbLoginButtonLoading}
+        >
+        <i className='fa fa-facebook-official' aria-hidden='true'></i>
+        {'  '}Log in with Facebook
+        </Button>
+      </div>
+      //   </MuiThemeProvider>
+      // </div>
     )
   }
 }
