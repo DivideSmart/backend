0667259d2973897e204eeb6d5910dda861f80a91
Luo Yuyang <royl8@qq.com>
2018-09-24 12:26:44 +0800

    update Create Bill UI

--- dividesmart/main/static/components/create_form.jsx
+++ dividesmart/main/static/components/create_form.jsx
@@ -22,7 +22,12 @@ import ReceiptButton from './material/receipt_float_btn.jsx'
 import { createForm } from 'rc-form';
 import '../style/index.less'
 const RadioItem = Radio.RadioItem;
-const CheckboxItem = Checkbox.CheckboxItem;
+const CheckboxItem = Checkbox.CheckboxItem
+
+
+const Item = List.Item
+const Brief = Item.Brief
+
 
 const AgreeItem = Checkbox.AgreeItem;
 const myImg = src => < img src={`https://gw.alipayobjects.com/zos/rmsportal/${src}.svg`} className="am-icon am-icon-xs" style={{ width: 60, height: 60 }} alt="" />;
@@ -34,8 +39,10 @@ import MList from '@material-ui/core/List';
 import MListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import MCheckbox from '@material-ui/core/Checkbox';
 import Avatar from '@material-ui/core/Avatar';
+import AddIcon from '@material-ui/icons/Add';
 import Paper from '@material-ui/core/Paper';
 import Divider from '@material-ui/core/Divider';
 
@@ -47,6 +54,9 @@ import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
+import {Link} from "react-router-dom";
+import PersonIcon from '@material-ui/icons/Person';
+import ListSubheader from '@material-ui/core/ListSubheader';
 
 
 let moneyKeyboardWrapProps;
@@ -65,6 +75,27 @@ const tabs = [
   { title: 'Type ' },
   { title: 'Scan Receipt' },
 ];
+
+  var friendsYouOwe = [
+    {
+      key: '1',
+      name: 'Harry',
+      avatarUrl: 'https://cactusthemes.com/blog/wp-content/uploads/2018/01/tt_avatar_small.jpg',
+      acc: 10.28,
+    }, {
+      key: '2',
+      name: 'Charlie',
+      avatarUrl: 'https://www.shareicon.net/data/256x256/2016/07/05/791216_people_512x512.png',
+      acc: 20.66,
+    }, {
+      key: '3',
+      name: 'Oscar',
+      avatarUrl: 'https://www.osustuff.org/img/avatars/2017-04-22/211652.jpg',
+      acc: 8.6,
+    },
+  ]
+
+const emails = ['Harry', 'Oscar'];
 
 
 class H5NumberInputExample extends React.Component {
@@ -108,40 +139,60 @@ class H5NumberInputExample extends React.Component {
         <WhiteSpace size="lg" />
 
 
-        <div >
-          <Paper elevation={0}>
-          <MList>
-            {[0, 1, 2].map(value => (
-              <div key={value}>
-              <MListItem key={value} dense button>
-                <Avatar alt="Remy Sharp" style={{width: 38, height: 38}} src="https://forums.dctp.ws/download/file.php?avatar=10907_1408814803.gif" />
-                <ListItemText primary={`Line item ${value + 1}`} />
-                <ListItemSecondaryAction>
-
-                  <Input
-                    id="adornment-amount"
-                    // value={this.state.amount}
-                    // onChange={this.handleChange('amount')}
-                    style={{width: '16vw'}}
-                    startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                  />
-
-                </ListItemSecondaryAction>
-              </MListItem>
-                </div>
-            ))}
-          </MList>
-          </Paper>
-        </div>
-
-
+        {/*<div >*/}
+          {/*<Paper elevation={0}>*/}
+            {/*<MList>*/}
+              {/*{[0, 1, 2].map(value => (*/}
+                {/*<div key={value}>*/}
+                  {/*<MListItem key={value} dense button>*/}
+                    {/*<Avatar alt="Remy Sharp" style={{width: 38, height: 38}} src="https://forums.dctp.ws/download/file.php?avatar=10907_1408814803.gif" />*/}
+                    {/*<ListItemText primary={`Line item ${value + 1}`} />*/}
+                    {/*<ListItemSecondaryAction>*/}
+        
+                      {/*<Input*/}
+                        {/*id="adornment-amount"*/}
+                        {/*// value={this.state.amount}*/}
+                        {/*// onChange={this.handleChange('amount')}*/}
+                        {/*style={{width: '16vw'}}*/}
+                        {/*startAdornment={<InputAdornment position="start">$</InputAdornment>}*/}
+                      {/*/>*/}
+        
+                    {/*</ListItemSecondaryAction>*/}
+                  {/*</MListItem>*/}
+                {/*</div>*/}
+              {/*))}*/}
+            {/*</MList>*/}
+          {/*</Paper>*/}
+        {/*</div>*/}
 
 
-
-
-
-
-        <WhiteSpace size="lg" />
+        {/*<div>*/}
+          {/*<List renderHeader={() => 'test'} className="my-list">*/}
+            {/*{*/}
+              {/*friendsYouOwe.map(friend => (<List.Item*/}
+              {/*key={friend.key}*/}
+              {/*thumb={*/}
+                {/*<Badge>*/}
+                  {/*<span*/}
+                    {/*style={{*/}
+                      {/*width: '44px',*/}
+                      {/*height: '44px',*/}
+                      {/*background: 'url(' + friend.avatarUrl + ') center center /  44px 44px no-repeat',*/}
+                      {/*display: 'inline-block' }}*/}
+                  {/*/>*/}
+                {/*</Badge>*/}
+              {/*}*/}
+              {/*multipleLine*/}
+              {/*// onClick={() => { window.location.href = '/u/1'}}*/}
+              {/*extra={*/}
+                {/*<span className={'other-owe-amount'}>${ friend.acc }</span>*/}
+              {/*}*/}
+            {/*>*/}
+              {/*{friend.name} <Brief></Brief>*/}
+            {/*</List.Item>))*/}
+            {/*}*/}
+          {/*</List>*/}
+        {/*</div>*/}
 
         <List>
           <InputItem
@@ -153,6 +204,41 @@ class H5NumberInputExample extends React.Component {
         </List>
 
         <WhiteSpace size="lg" />
+
+        <WhiteSpace size="lg" />
+        <List renderHeader={() => 'Split by'}>
+        </List>
+        <Paper elevation={0}>
+          <MList>
+            {emails.map(email => (
+              <MListItem button key={email}>
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="https://forums.dctp.ws/download/file.php?avatar=10907_1408814803.gif" />
+                </ListItemAvatar>
+                <ListItemText style={{float: 'right'}}  primary={email} />
+                <ListItemSecondaryAction>
+                  <Input
+                    id="adornment-amount"
+                    // value={this.state.amount}
+                    // onChange={this.handleChange('amount')}
+                    style={{width: '16vw', marginRight: '6vw'}}
+                    startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                  />
+                </ListItemSecondaryAction>
+              </MListItem>
+            ))}
+            <MListItem button >
+              <ListItemAvatar>
+                <Avatar>
+                  <AddIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="add people" />
+            </MListItem>
+          </MList>
+        </Paper>
+
+
         {/* <Tabs tabs={tabs}
           initialPage={1}
           onChange={(tab, index) => { console.log('onChange', index, tab); }}
@@ -166,24 +252,24 @@ class H5NumberInputExample extends React.Component {
           </div>
         </Tabs> */}
 
-        <List className={'divide-list'}>
-            {[0, 1, 2].map(value => (
-              <InputItem
-                key={value}
-                {...getFieldProps('money3')}
-                type={type}
-                defaultValue={100}
-                placeholder="start from left"
-                clear
-                moneyKeyboardAlign="left"
-                moneyKeyboardWrapProps={moneyKeyboardWrapProps}
-                extra="$"
-              >
-                <Avatar alt="Remy Sharp" src="https://forums.dctp.ws/download/file.php?avatar=10907_1408814803.gif" />
-              </InputItem>
-            ))}
+        {/*<List className={'divide-list'}>*/}
+            {/*{[0, 1, 2].map(value => (*/}
+              {/*<InputItem*/}
+                {/*key={value}*/}
+                {/*{...getFieldProps('money3')}*/}
+                {/*type={type}*/}
+                {/*defaultValue={100}*/}
+                {/*placeholder="start from left"*/}
+                {/*clear*/}
+                {/*moneyKeyboardAlign="left"*/}
+                {/*moneyKeyboardWrapProps={moneyKeyboardWrapProps}*/}
+                {/*extra="$"*/}
+              {/*>*/}
+                {/*<Avatar alt="Remy Sharp" src="https://forums.dctp.ws/download/file.php?avatar=10907_1408814803.gif" />*/}
+              {/*</InputItem>*/}
+            {/*))}*/}
 
-        </List>
+        {/*</List>*/}
 
         <WhiteSpace size="lg" />
         <List>
