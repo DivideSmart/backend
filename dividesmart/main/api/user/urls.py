3f7740e3ecdb9ba196b1f54cd41690fca64148e3
Edmund Mok <edmundmok@outlook.com>
2018-09-24 11:40:34 +0800

    Update /api/users/{user_id} to /api/user/

--- dividesmart/main/api/user/urls.py
+++ dividesmart/main/api/user/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.user),
+    path('friends/', views.friends),
+    path('friends/<uuid:friend_id>/', views.friend),
+    path('friends/<uuid:friend_id>/entries/', views.friend_entries),
+    path('friends/<uuid:friend_id>/bills/', views.friend_bills),
+    path('friends/<uuid:friend_id>/bills/<uuid:bill_id>/', views.friend_bill),
+    path('friends/<uuid:friend_id>/payments/', views.friend_payments),
+    path('friends/<uuid:friend_id>/payments/<uuid:payment_id>/', views.friend_payment),
+    path('groups/', views.groups)
 ]
--- dividesmart/main/api/users/urls.py
+++ dividesmart/main/api/users/urls.py
@@ -3,13 +3,4 @@ from . import views
 
 urlpatterns = [
     path('<uuid:user_id>/', views.user),
-    path('<uuid:user_id>/friends/', views.friends),
-    path('<uuid:user_id>/friends/<uuid:friend_id>/', views.friend),
-    path('<uuid:user_id>/friends/<uuid:friend_id>/entries/', views.friend_entries),
-    path('<uuid:user_id>/friends/<uuid:friend_id>/bills/', views.friend_bills),
-    path('<uuid:user_id>/friends/<uuid:friend_id>/bills/<uuid:bill_id>/', views.friend_bill),
-    path('<uuid:user_id>/friends/<uuid:friend_id>/payments/', views.friend_payments),
-    path('<uuid:user_id>/friends/<uuid:friend_id>/payments/<uuid:payment_id>/',
-         views.friend_payment),
-    path('<uuid:user_id>/groups/', views.groups)
 ]
