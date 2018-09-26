5c10b30861f34685cbd5f7d2c00038779ad241af
royl8 <royl8@qq.com>
2018-09-26 18:45:26 +0800

    update

--- dividesmart/main/static/service-worker.js
+++ dividesmart/main/static/service-worker.js
@@ -1,26 +1,43 @@
-var CACHE_NAME = 'we-share-cache';
 
-var urlsToCache = [
-  '/',
-  '/styles/styles.css',
-  '/script/webpack-bundle.js'
-];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        // Open a cache and cache our files
-        return cache.addAll(urlsToCache);
-      })
-  );
+const version = "0.0.1"
+const cacheName = `weshare-${version}`
+
+self.addEventListener('install', e => {
+  caches.delete(cacheName);
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('install service worker and cache data')
+      return cache.addAll([
+        `/`,
+        // `/login/`,
+        `/static/bundle/main/index.js`,
+        // `/static/bundle/main/login.js`,
+      ]).then(() => self.skipWaiting())
+    })
+  )
+})
+
+self.addEventListener('activate', event => {
+  console.log('active and control this domain')
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
+self.addEventListener('fetch', event => {
+  console.log(event.request.url)
+  if (!event.request.url.includes('login')) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
-});
+      caches.open(cacheName).then(cache => cache.match(event.request))
+      .then(response => {
+        if (response) {
+          console.log('hit')
+          return response
+        }
+        else {
+          console.log('not hit')
+          return fetch(event.request);
+        }
+      })
+    )
+  }
+})
