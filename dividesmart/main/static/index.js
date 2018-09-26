aeba6df804e08816071aaf70e354ef87075992e2
royl8 <royl8@qq.com>
2018-09-26 10:30:29 +0800

    fix warnings

--- dividesmart/main/static/index.js
+++ dividesmart/main/static/index.js
@@ -74,16 +74,16 @@ class App extends React.Component {
     this.findGroupMembers = this.findGroupMembers.bind(this);
 
     // Check for browser support of service worker
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('./service-worker.js')
-      .then(function(registration) {
-        // Successful registration
-        console.log('Hooray. Registration successful, scope is:', registration.scope);
-      }).catch(function(err) {
-        // Failed registration, service worker won’t be installed
-        console.log('Whoops. Service worker registration failed, error:', error);
-      });
-    }
+    // if ('serviceWorker' in navigator) {
+    //   navigator.serviceWorker.register('./service-worker.js')
+    //   .then(function(registration) {
+    //     // Successful registration
+    //     console.log('Hooray. Registration successful, scope is:', registration.scope);
+    //   }).catch(function(error) {
+    //     // Failed registration, service worker won’t be installed
+    //     console.log('Whoops. Service worker registration failed, error:', error);
+    //   });
+    // }
   }
 
   componentWillMount() {
